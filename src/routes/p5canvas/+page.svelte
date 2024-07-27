ddf09d8919dd69bd7428edf38ed4161c02b977bf
zuzokim <zuzokim@gmail.com>
2024-07-27 20:23:00 +0900

    add p5js canvas for testing

---
+++ src/routes/p5canvas/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  import P5canvas from './P5canvas.svelte';
+</script>
+
+<main>
+  <h1>P5canvas with Svelte</h1>
+  <P5canvas />
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+</style>
+
+<svelte:head>
+  <title>P5canvas with Svelte</title>
+  <meta name="description" content="P5canvas with Svelte" />
+</svelte:head>
